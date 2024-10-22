58ad20594ff6969c458d7fab24736cf290aad599
Noctwo <mr.evok@gmail.com>
2024-10-22 10:58:19 +0200

    darkmode css progress - wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './Components/Footer/Footer'
-import Header from './Components/Header/Header'
-import Home from './Pages/Home/Home'
-import Detail from './Pages/Detail/Detail'
-import LoginPage from './Pages/LoginPage/LoginPage'
-import { UserProvider } from './Context/UserContext'
-import Signup from './Pages/Signup/Signup'
-import Profile from './Pages/Profile/Profile'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Detail from "./Pages/Detail/Detail";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import { UserProvider } from "./Context/UserContext";
+import Signup from "./Pages/Signup/Signup";
+import Profile from "./Pages/Profile/Profile";
+import { useState } from "react";
 
 function App() {
+  const [dark, setDark] = useState<boolean>(false);
 
   return (
-    <UserProvider>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/profile/:id" element={<Profile />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/recipe/:id" element ={<Detail />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-    </UserProvider>
-  )
+    <div className={`${dark && "dark-mode-on"}`}>
+      <UserProvider>
+        <BrowserRouter>
+          <Header dark={dark} setDark={setDark} />
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/recipe/:id" element={<Detail />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </UserProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;

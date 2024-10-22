f421a8abf2ec51904f1d8374ad57a009b705cb72
Noctwo <mr.evok@gmail.com>
2024-10-22 10:27:27 +0200

    darkmode start

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,54 +1,75 @@
 import { Link } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
 import { useUserContext } from "../../Context/UserContext";
 import { supabaseClient } from "../../lib/supabaseClient";
 
-
-const Header = () => {
-
-    const userContext = useUserContext();
-    const user = userContext?.user;
-
-    const handleLogoutClicked = async (e:React.MouseEvent) => {
-        e.preventDefault();
-
-        const signoutResponse = await supabaseClient.auth.signOut();
-
-        if (signoutResponse.error) {
-            console.error("Logout Error", signoutResponse.error)
-        } else {
-            userContext?.setUser(null);
-        }
-    };
-
-    return ( 
-
-    <header>
-        <nav>
-            <Link to={"/"}>
-            <div className="header-logo">
-                <img src="Logo.svg" alt="company logo"/>
-            </div>
-            </Link>
-            <div className="nav-link-container">
-            <Link to={"/"}>Home</Link>
-            <a href="#">Rezepte</a>
-            <a href="#">Ueber uns</a>
-            </div>
-            
-                {user ? (
-                    <div className="login-status-container">
-                    <span>Wilkommen, </span>
-                    <Link to={`/profile/${user.id}`}><span>{user.email}</span></Link>
-                    <button className="logout-button" onClick={handleLogoutClicked}>Logout</button>
-                    </div>
-                ): (
-                    <button><Link to="/login">Login</Link></button>
-                )}
-            
-        </nav>
-    </header>
-    );
+interface HeaderProps {
+  dark: boolean;
+  setDark: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+const Header: React.FC<HeaderProps> = (props) => {
+  const darkModeFunction = () => {
+    props.setDark((set) => !set);
+  };
+
+  const userContext = useUserContext();
+  const user = userContext?.user;
+
+  const handleLogoutClicked = async (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    const signoutResponse = await supabaseClient.auth.signOut();
+
+    if (signoutResponse.error) {
+      console.error("Logout Error", signoutResponse.error);
+    } else {
+      userContext?.setUser(null);
+    }
+  };
+
+  return (
+    <header>
+      <nav>
+        <Link to={"/"}>
+          <div className="header-logo">
+            <img src="Logo.svg" alt="company logo" />
+          </div>
+        </Link>
+        <div className="nav-link-container">
+          <Link to={"/"}>Home</Link>
+          <a href="#">Rezepte</a>
+          <a href="#">Ueber uns</a>
+        </div>
+
+        {user ? (
+          <div className="login-status-container">
+            <span>Wilkommen, </span>
+            <Link to={`/profile/${user.id}`}>
+              <span>{user.email}</span>
+            </Link>
+            <button className="logout-button" onClick={handleLogoutClicked}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <button>
+            <Link to="/login">Login</Link>
+          </button>
+        )}
+        <div className="dark-icon-wrapper">
+          <svg
+            onClick={darkModeFunction}
+            className="dark-icon"
+            viewBox="0 0 512 512"
+          >
+            <path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z" />
+            {props.dark ? "Light" : "Dark"}
+          </svg>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
